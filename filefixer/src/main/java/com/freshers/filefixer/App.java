183df05297fd553fe262264e0f418fd99d723153
rajsadho <rajsadho@users.noreply.github.com>
2021-11-14 17:25:16 -0400

    Setup Maven template for the project

---
+++ filefixer/src/main/java/com/freshers/filefixer/App.java
@@ -0,0 +1,13 @@
+package com.freshers.filefixer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
