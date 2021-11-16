42d16b8369464a212ed27e0982288dfb13e9875b
cumberson97 <60389903+cumberson97@users.noreply.github.com>
2021-11-15 22:33:23 -0400

    We good

--- filefixer/src/main/java/com/freshers/filefixer/App.java
+++ filefixer/src/main/java/com/freshers/filefixer/App.java
@@ -4,10 +4,8 @@ package com.freshers.filefixer;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello fix World!");
     }
 }
