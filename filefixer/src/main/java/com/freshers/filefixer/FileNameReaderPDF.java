eab8092bfcb8116957151aa085c8bde781a06e53
shane12345601 <shane12345601@gmail.com>
2021-11-17 01:25:17 -0400

    added code to read folder and store PDF's in ArrayList

--- filefixer/src/main/java/com/freshers/filefixer/FileNameReaderPDF.java
+++ filefixer/src/main/java/com/freshers/filefixer/FileNameReaderPDF.java
@@ -1,5 +1,37 @@
 package com.freshers.filefixer;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class FileNameReaderPDF {
-    
+    private ArrayList<PDF> pdfs;
+    File folder;
+
+    public FileNameReaderPDF(){
+        pdfs = new ArrayList<PDF>();
+    }
+
+    public int readData(){
+        int counter=0;
+
+        String path = "C:/Users/shane/Desktop/UWI/Year 3/Semester 1/COMP 3607/Sample files and CSVs-20211114/sample3/sample3/";
+
+        folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for(File f : listOfFiles){
+            if(f.getName().endsWith(".pdf")){
+                PDF pdf = new PDF(f);
+                pdfs.add(pdf);
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public ArrayList<PDF> getPdfs() {
+        return pdfs;
+    }
+
 }
