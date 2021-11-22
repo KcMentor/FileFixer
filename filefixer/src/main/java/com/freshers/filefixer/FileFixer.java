8f2d777b4ae8b4fe98a56b5c72c956d0c6b790dc
cumberson97 <60389903+cumberson97@users.noreply.github.com>
2021-11-22 14:37:39 -0400

    Filefixer class fixed

--- filefixer/src/main/java/com/freshers/filefixer/FileFixer.java
+++ filefixer/src/main/java/com/freshers/filefixer/FileFixer.java
@@ -1,5 +1,17 @@
 package com.freshers.filefixer;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+
 public class FileFixer {
-    
+    private ArrayList<Search> Searches;
+    private ArrayList<Record> Records;
+    private ArrayList<PDF> PDFs;
+    private ReaderCSV readerCSV;
+    private FileNameReaderPDF readerPDF;
+
+    public FileFixer() {
+
+    }
+
 }
