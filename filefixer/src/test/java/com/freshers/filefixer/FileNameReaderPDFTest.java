51fa9e207498411318e09055993b51c3da3d7041
rajsadho <rajsadho@users.noreply.github.com>
2021-11-24 16:11:39 -0400

    SearchStudentID tests

--- filefixer/src/test/java/com/freshers/filefixer/FileNameReaderPDFTest.java
+++ filefixer/src/test/java/com/freshers/filefixer/FileNameReaderPDFTest.java
@@ -3,7 +3,6 @@ package com.freshers.filefixer;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
--- filefixer/src/test/java/com/freshers/filefixer/ReaderCSVTest.java
+++ filefixer/src/test/java/com/freshers/filefixer/ReaderCSVTest.java
@@ -1,5 +1,13 @@
 package com.freshers.filefixer;
 
-public class ReaderCSVTest {
-    
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+
+import org.assertj.core.api.WithAssertions;
+
+public class ReaderCSVTest implements WithAssertions{
+    String repoDir = "./src/test/java/com/freshers/filefixer/testData/fileRepo".replace("/", File.separator);
+       
 }
