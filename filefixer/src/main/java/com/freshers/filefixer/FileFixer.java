e1a79a4998a738f73d3f0937eda21150e0d9f342
rajsadho <rajsadho@users.noreply.github.com>
2021-11-24 17:25:07 -0400

    ReaderCSV tests added

--- filefixer/src/main/java/com/freshers/filefixer/FileFixer.java
+++ filefixer/src/main/java/com/freshers/filefixer/FileFixer.java
@@ -1,7 +1,6 @@
 package com.freshers.filefixer;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.ListIterator;
 import java.io.BufferedWriter;
--- filefixer/src/test/java/com/freshers/filefixer/ReaderCSVTest.java
+++ filefixer/src/test/java/com/freshers/filefixer/ReaderCSVTest.java
@@ -5,20 +5,63 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.assertj.core.api.WithAssertions;
 
 public class ReaderCSVTest implements WithAssertions{
     String repoDir = "./src/test/java/com/freshers/filefixer/testData/fileRepo".replace("/", File.separator);
+    String testDir = "./src/test/java/com/freshers/filefixer/testData/testFiles".replace("/", File.separator);
+    Path testPath = Paths.get(testDir).toAbsolutePath().normalize();
+    Path repoPath = Paths.get(repoDir).toAbsolutePath().normalize();
     String csvName = "Test.csv";
+
+
+    @Test void readFromDirectoryWithCSV () {
+        ReaderCSV reader = new ReaderCSV();
+        File file = reader.getCSVFile(repoDir);
+        assertThat(file).isNotNull();
+    }
+
+    @Test void readFromDirectoryWithoutCSV () {
+        ReaderCSV reader = new ReaderCSV();
+        File file = reader.getCSVFile("./src/test/java/com/freshers/filefixer/testData/renamedFiles".replace("/", File.separator));
+        assertThat(file).isNull();
+    }
+
+    
+
+    @Test void readFromDirectoryWithoutMoreThanOneCSV () {
+        ReaderCSV reader = new ReaderCSV();
+        try {
+            Files.copy(Paths.get(repoDir, "Test.csv"), Paths.get(testDir, "Test.csv"));
+            Files.copy(Paths.get(repoDir, "Test.csv"), Paths.get(testDir, "Copy.csv"));
+        } catch (IOException e) {
+            assert false;
+        }
+        
+        File file = reader.getCSVFile("./src/test/java/com/freshers/filefixer/testData".replace("/", File.separator));
+        
+        try {
+            Files.delete(Paths.get(testDir, "Test.csv"));
+            Files.delete(Paths.get(testDir, "Copy.csv"));
+        } catch (IOException e) {
+            assert false;
+        }
+
+        assertThat(file).isNull();
+    }    
+
     @Test
     public void testReadFromCSV () {
         ReaderCSV reader = new ReaderCSV();
 
-        int rowCount = reader.readData(Paths.get(repoDir, csvName).toString());
+        int rowCount = reader.readData(repoDir);
 
-        assertThat(rowCount).isEqualTo(3);
-        
+        assertThat(rowCount).isEqualTo(2);
+
     }
 }
